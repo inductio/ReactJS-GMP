69e9061654cd1a4ffbe4ba5d1fa6beda6f983600
Oleksandr_Shustrov <oleksandr_shustrov@epam.com>
2021-03-01 09:52:43 +0200

    ReactJS-GMP-Task3

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../styles/common.scss';
-import Header from "./header";
-import Main from './main';
+import Header from "./Header";
+import Main from './Main';
 import Footer from './Footer';
 import ErrorBoundary from './common/ErrorBoundary';
 
