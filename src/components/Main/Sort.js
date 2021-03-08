d49132cacb244d4548746bd52ae42c2b00297240
Oleksandr_Shustrov <oleksandr_shustrov@epam.com>
2021-03-08 23:45:11 +0200

    GMP React - task4 - default sorting

--- src/components/Main/Sort.js
+++ src/components/Main/Sort.js
@@ -5,6 +5,9 @@ class Sort extends React.Component {
     constructor(props) {
         super(props);
         this.onSort = this.onSort.bind(this);
+
+        //Default sorting
+        this.props.sort(this.sortByDate(this.props.movies));
     }
 
     onSort(e) {
@@ -19,6 +22,10 @@ class Sort extends React.Component {
             movies = this.sortByRating(this.props.movies)
         }
 
+        if (value === 'Runtime') {
+            movies = this.sortByRuntime(this.props.movies)
+        }
+
         this.props.sort(movies)
     }
 
@@ -30,12 +37,18 @@ class Sort extends React.Component {
 
     sortByRating(movies) {
         return movies.sort((a, b) => {
-            return new Date(b.vote_average) - new Date(a.vote_average);
+            return b.vote_average - a.vote_average;
+        })
+    }
+
+    sortByRuntime(movies) {
+        return movies.sort((a, b) => {
+            return b.runtime - a.runtime;
         })
     }
 
     render() {
-        const options = ['Release Date', 'Rating'];
+        const options = ['Release Date', 'Rating', 'Runtime'];
         return (
             <div className="sort">
                 <label htmlFor="sort__select" className="sort__label">Sort By</label>
