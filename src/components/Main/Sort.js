4474c07e0589c313560df9c9017d0bf7d8932355
Oleksandr_Shustrov <oleksandr_shustrov@epam.com>
2021-03-08 21:21:18 +0200

    ReactJS-GMP-Task4 - sorting

--- src/components/Main/Sort.js
+++ src/components/Main/Sort.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import '../../styles/sort.scss';
 
-class Sort extends React.PureComponent {
+class Sort extends React.Component {
+    constructor(props) {
+        super(props);
+        this.onSort = this.onSort.bind(this);
+    }
+
+    onSort(e) {
+        let movies;
+        const value = e.target.value;
+
+        if (value === 'Release Date') {
+            movies = this.sortByDate(this.props.movies)
+        }
+
+        if (value === 'Rating') {
+            movies = this.sortByRating(this.props.movies)
+        }
+
+        this.props.sort(movies)
+    }
+
+    sortByDate(movies) {
+        return movies.sort((a, b) => {
+            return new Date(a.release_date) - new Date(b.release_date);
+        })
+    }
+
+    sortByRating(movies) {
+        return movies.sort((a, b) => {
+            return new Date(b.vote_average) - new Date(a.vote_average);
+        })
+    }
+
     render() {
         const options = ['Release Date', 'Rating'];
         return (
             <div className="sort">
                 <label htmlFor="sort__select" className="sort__label">Sort By</label>
-                <select name="sort" id="sort__select" className="sort__select">
+                <select name="sort" id="sort__select" className="sort__select" onChange={this.onSort}>
                     {options.map(function (option, index) {
                         return <option key={index} name={option}>{option}</option>
                     })}
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -8,17 +8,34 @@ import '../../styles/main.scss';
 import '../../styles/movies.scss';
 import '../../styles/menu.scss';
 
-class Main extends React.PureComponent {
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies: movies
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+        this.sortMovies = this.sortMovies.bind(this);
+    }
+
+    toggleModal(isModalActive) {
+        this.setState({isModalActive});
+    }
+
+    sortMovies(movies) {
+        this.setState({movies})
+    }
+
     render() {
         return (
             <main className="main">
                 <div className="menu">
                     <Filter/>
-                    <Sort/>
+                    <Sort sort={this.sortMovies.bind(this)} movies={this.state.movies}/>
                 </div>
                 <div className="movies">
                     <MoviesCounter counter={movies.length} />
-                    <MoviesGrid movies={movies}/>
+                    <MoviesGrid movies={this.state.movies}/>
                 </div>
             </main>
         )
