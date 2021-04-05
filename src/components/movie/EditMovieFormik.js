8f096c16b1c69ccd119aa39e0cd86b837892cb89
Oleksandr_Shustrov <oleksandr_shustrov@epam.com>
2021-04-05 23:59:15 +0300

    GMP React - task7 - formik

--- src/components/movie/EditMovieFormik.js
+++ src/components/movie/EditMovieFormik.js
@@ -3,6 +3,7 @@ import { Formik, Form, Field } from 'formik';
 import { editMovieRequest } from '../../actions';
 import { connect } from 'react-redux';
 import _ from 'lodash';
+import * as Yup from "yup";
 
 const EditMovieFormik = (props) => {
     if (!props.movie) {
@@ -38,56 +39,79 @@ const EditMovieFormik = (props) => {
             </h3>
 
             <Formik
-                initialValues = {editFormConfig.initialValues}
+                initialValues={editFormConfig.initialValues}
                 onSubmit={async values => editFormConfig.onSubmit(values)}
+                validationSchema={Yup.object().shape({
+                    title: Yup.string().required("Required"),
+                    poster_path: Yup.string().required("Required"),
+                    overview: Yup.string().required("Required"),
+                    runtime: Yup.number().required("Required")
+                })}
             >
-                <Form className="movie-form__form">
-                    {editFormConfig.descriptions.map((description, index) => (
-                        <p key={index} className={description.className}>
-                            {description.text}
-                        </p>
-                    ))}
+                {(props) => {
+                    const {
+                        values,
+                        touched,
+                        errors
+                    } = props;
+                    return (
+                        <Form className="movie-form__form">
+                            {editFormConfig.descriptions.map((description, index) => (
+                                <p key={index} className={description.className}>
+                                    {description.text}
+                                </p>
+                            ))}
 
-                    <label htmlFor="movieTitle" className="movie-form__label">Title</label>
-                    <Field id="movieTitle" name="title" type="text" placeholder="Add Movie\'s Name Here" className="movie-form__input"/>
+                            <label htmlFor="movieTitle" className="movie-form__label">Title</label>
+                            <Field id="movieTitle" name="title" type="text" placeholder="Add Movie\'s Name Here"
+                                   className={errors.title && touched.title ? "movie-form__input error" : "movie-form__input"}
+                            />
 
-                    <label htmlFor="movieDate" className="movie-form__label">Release date</label>
-                    <Field id="movieDate" name="release_date" type="date" placeholder="Select Date" className="movie-form__input"/>
+                            <label htmlFor="movieDate" className="movie-form__label">Release date</label>
+                            <Field id="movieDate" name="release_date" type="date" placeholder="Select Date"
+                                   className="movie-form__input"
+                            />
 
-                    <label htmlFor="movieUrl" className="movie-form__label">Movie URL</label>
-                    <Field id="movieUrl" name="poster_path" type="text" placeholder="Add Movie\'s Name Here" className="movie-form__input"/>
+                            <label htmlFor="movieUrl" className="movie-form__label">Movie URL</label>
+                            <Field id="movieUrl" name="poster_path" type="text" placeholder="Add Movie\'s Name Here"
+                                   className={errors.title && touched.title ? "movie-form__input error" : "movie-form__input"}
+                            />
 
-                    <div id="checkbox-group">Select Genre</div>
+                            <div id="checkbox-group">Select Genre</div>
 
-                    <div role="group" aria-labelledby="checkbox-group">
-                        {props.movie.genres.map(genre =>
-                            (
-                                <label>
-                                    <Field type="checkbox" name="genres" value={genre}/>
-                                    {genre}
-                                </label>
-                            )
-                        )}
-                    </div>
+                            <div role="group" aria-labelledby="checkbox-group">
+                                {values.genres.map((genre, idx) =>
+                                    (
+                                        <label key={idx}>
+                                            <Field type="checkbox" name="genres" value={genre}/>
+                                            {genre}
+                                        </label>
+                                    )
+                                )}
+                            </div>
 
-                    <label htmlFor="movieOverview" className="movie-form__label">Overview</label>
-                    <Field id="movieOverview" name="overview" type="text" placeholder="Overview here" className="movie-form__input"/>
+                            <label htmlFor="movieOverview" className="movie-form__label">Overview</label>
+                            <Field id="movieOverview" name="overview" type="text" placeholder="Overview here"
+                                   className="movie-form__input"/>
 
-                    <label htmlFor="movieRuntime" className="movie-form__label">Runtime</label>
-                    <Field id="movieRuntime" name="runtime" type="number" placeholder="Runtime here" className="movie-form__input"/>
+                            <label htmlFor="movieRuntime" className="movie-form__label">Runtime</label>
+                            <Field id="movieRuntime" name="runtime" type="number" placeholder="Runtime here"
+                                   className="movie-form__input"/>
 
-                    <div className="movie-form__buttons">
-                        {editFormConfig.buttons.map(button => (
-                            <button
-                                key={`${button.title}`}
-                                type={button.type}
-                                className={`movie-form__btn movie-form__${button.type}`}
-                            >
-                                {button.title}
-                            </button>
-                        ))}
-                    </div>
-                </Form>
+                            <div className="movie-form__buttons">
+                                {editFormConfig.buttons.map(button => (
+                                    <button
+                                        key={`${button.title}`}
+                                        type={button.type}
+                                        className={`movie-form__btn movie-form__${button.type}`}
+                                    >
+                                        {button.title}
+                                    </button>
+                                ))}
+                            </div>
+                        </Form>
+                    )
+                }}
             </Formik>
         </div>
     )
