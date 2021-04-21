f95ee931e1098494f3943843880a9cc7e4faa74a
Oleksandr_Shustrov <oleksandr_shustrov@epam.com>
2021-04-22 02:47:56 +0300

    GMP React - task9 - tests fix

--- __tests__/components/AddMovieForm.test.js
+++ __tests__/components/AddMovieForm.test.js
@@ -8,12 +8,13 @@ import Adapter from 'enzyme-adapter-react-17-updated';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Add Movie Form', () => {
+    const tree = mount(
+        <Provider store={ store }>
+            <AddMovieFormik/>
+        </Provider>
+    );
+
     it('Should update title field', async () => {
-        const tree = mount(
-            <Provider store={ store }>
-                <AddMovieFormik/>
-            </Provider>
-        );
         const titleInput = tree.find('input[name="title"]').first();
         await act(async () => {
              titleInput.simulate('change', {
@@ -25,5 +26,147 @@ describe('Add Movie Form', () => {
         });
 
         expect(titleInput.html()).toMatch('Test Title');
+    });
+
+    it('Should update date field', async () => {
+        const dateInput = tree.find('input[name="release_date"]').first();
+        await act(async () => {
+            dateInput.simulate('change', {
+                target: {
+                    name: 'release_date',
+                    value: '2021-04-01'
+                }
+            });
+        });
+
+        expect(dateInput.html()).toMatch('2021-04-01');
+    });
+
+    it('Should update poster path field', async () => {
+        const posterPathInput = tree.find('input[name="poster_path"]').first();
+        await act(async () => {
+            posterPathInput.simulate('change', {
+                target: {
+                    name: 'poster_path',
+                    value: '//test.img'
+                }
+            });
+        });
+
+        expect(posterPathInput.html()).toMatch('//test.img');
+    });
+
+    it('Should update overview field', async () => {
+        const overviewInput = tree.find('input[name="overview"]').first();
+        await act(async () => {
+            overviewInput.simulate('change', {
+                target: {
+                    name: 'overview',
+                    value: 'Test Overview'
+                }
+            });
+        });
+
+        expect(overviewInput.html()).toMatch('Test Overview');
+    });
+
+    it('Should update runtime field', async () => {
+        const runtimeInput = tree.find('input[name="runtime"]').first();
+        await act(async () => {
+            runtimeInput.simulate('change', {
+                target: {
+                    name: 'runtime',
+                    value: 132
+                }
+            });
+        });
+
+        expect(runtimeInput.html()).toMatch('132');
+    });
+
+    it('Should call submit handler on form submit', async () => {
+        const dispatch = jest.fn();
+
+        const mountedStore = {
+            ...store,
+            dispatch
+        };
+
+        const tree = mount(
+            <Provider store={ mountedStore }>
+                <AddMovieFormik/>
+            </Provider>
+        );
+
+        const runtimeInput = tree.find('input[name="runtime"]').first();
+        await act(async () => {
+            runtimeInput.simulate('change', {
+                target: {
+                    name: 'runtime',
+                    value: 111
+                }
+            });
+        });
+
+        const overviewInput = tree.find('input[name="overview"]').first();
+        await act(async () => {
+            overviewInput.simulate('change', {
+                target: {
+                    name: 'overview',
+                    value: 'Test Overview'
+                }
+            });
+        });
+
+        const dateInput = tree.find('input[name="release_date"]').first();
+        await act(async () => {
+            dateInput.simulate('change', {
+                target: {
+                    name: 'release_date',
+                    value: '2021-04-01'
+                }
+            });
+        });
+
+        const posterPathInput = tree.find('input[name="poster_path"]').first();
+        await act(async () => {
+            posterPathInput.simulate('change', {
+                target: {
+                    name: 'poster_path',
+                    value: '//test.img'
+                }
+            });
+        });
+
+        const titleInput = tree.find('input[name="title"]').first();
+        await act(async () => {
+            titleInput.simulate('change', {
+                target: {
+                    name: 'title',
+                    value: 'Test Title'
+                }
+            });
+        });
+
+        await act(async () => tree.find('form').simulate('submit');
+
+        expect(dispatch).toHaveBeenCalled();
+    });
+
+    it('Should reset fields on Reset btn click', async () => {
+        const runtimeInput = tree.find('input[name="runtime"]').first();
+        await act(async () => {
+            runtimeInput.simulate('change', {
+                target: {
+                    name: 'runtime',
+                    value: 111
+                }
+            });
+        });
+        expect(runtimeInput.html()).toMatch('111');
+
+        await act(async () => tree.find('.movie-form__reset').first().simulate('click'));
+
+        expect(runtimeInput.getDOMNode().value).toMatch('0');
     });
 });
